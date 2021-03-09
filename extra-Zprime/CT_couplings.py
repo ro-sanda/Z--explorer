5fd149c157ce10d2470f8c252b40dc57f92ffaf3
ro-sanda <63731604+ro-sanda@users.noreply.github.com>
2021-03-09 11:47:20 -0300

    Add files via upload

---
+++ extra-Zprime/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules =.2.4
+# Mathematica version: 10.2.0 for Linux x86 (64-bit) (July 28, 2015)
+# Date: Tue 2 Mar 2021 13:23:42
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
